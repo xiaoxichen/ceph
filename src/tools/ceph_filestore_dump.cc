cc6e43de0103619be3b40af4399363c92a8a2ade
David Zafman <david.zafman@inktank.com>
2014-05-19 18:33:24 -0700

    ceph_filestore_dump: Add rm-attr object command
    Signed-off-by: David Zafman <david.zafman@inktank.com>

--- src/tools/ceph_filestore_dump.cc
+++ src/tools/ceph_filestore_dump.cc
@@ -1327,6 +1327,20 @@ int do_set_attr(ObjectStore *store, coll_t coll, ghobject_t &ghobj, string key, 
   return 0;
 }
 
+int do_rm_attr(ObjectStore *store, coll_t coll, ghobject_t &ghobj, string key)
+{
+  ObjectStore::Transaction tran;
+  ObjectStore::Transaction *t = &tran;
+
+  if (debug)
+    cerr << "Rmattr " << ghobj << std::endl;
+
+  t->rmattr(coll, ghobj, key);
+
+  store->apply_transaction(*t);
+  return 0;
+}
+
 void usage(po::options_description &desc)
 {
     cerr << std::endl;
@@ -1761,6 +1775,13 @@ int main(int argc, char **argv)
 	if (vm.count("arg1") == 0 || vm.count("arg2") == 0)
 	  usage(desc);
 	r = do_set_attr(fs, coll, ghobj, arg1, arg2);
+	if (r)
+	  ret = 1;
+        goto out;
+      } else if (objcmd == "rm-attr") {
+	if (vm.count("arg1") == 0)
+	  usage(desc);
+	r = do_rm_attr(fs, coll, ghobj, arg1);
 	if (r)
 	  ret = 1;
         goto out;
