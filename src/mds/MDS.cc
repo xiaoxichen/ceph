f4d66e76548dda3f63e7a21a5b12918aa61c4882
Sage Weil <sage@inktank.com>
2014-05-29 13:08:59 -0700

    mds: use Connection::mark_down()
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mds/MDS.cc
+++ src/mds/MDS.cc
@@ -2224,13 +2224,13 @@ bool MDS::ms_handle_reset(Connection *con)
     if (session) {
       if (session->is_closed()) {
 	dout(3) << "ms_handle_reset closing connection for session " << session->info.inst << dendl;
-	messenger->mark_down(con);
+	con->mark_down();
 	con->set_priv(NULL);
 	sessionmap.remove_session(session);
       }
       session->put();
     } else {
-      messenger->mark_down(con);
+      con->mark_down();
     }
   }
   return false;
@@ -2253,7 +2253,7 @@ void MDS::ms_handle_remote_reset(Connection *con)
     if (session) {
       if (session->is_closed()) {
 	dout(3) << "ms_handle_remote_reset closing connection for session " << session->info.inst << dendl;
-	messenger->mark_down(con);
+	con->mark_down();
 	con->set_priv(NULL);
 	sessionmap.remove_session(session);
       }
