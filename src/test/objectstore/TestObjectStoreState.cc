04e310dfcb3b858de0828f3ed7ae482e06186c4d
Sage Weil <sage@redhat.com>
2015-03-08 15:38:17 -0700

    os: pass bits to create_collection()
    Some implementations will need to know the hash range that a PG
    covers.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/test/objectstore/TestObjectStoreState.cc
+++ src/test/objectstore/TestObjectStoreState.cc
@@ -38,7 +38,7 @@ void TestObjectStoreState::init(int colls, int objs)
   ObjectStore::Transaction *t;
   t = new ObjectStore::Transaction;
 
-  t->create_collection(META_COLL);
+  t->create_collection(META_COLL, 0);
   m_store->apply_transaction(*t);
 
   wait_for_ready();
@@ -51,7 +51,7 @@ void TestObjectStoreState::init(int colls, int objs)
         << " meta " << entry->m_meta_obj.oid.name << dendl;
 
     t = new ObjectStore::Transaction;
-    t->create_collection(entry->m_coll);
+    t->create_collection(entry->m_coll, 32);
     bufferlist hint;
     uint32_t pg_num = colls;
     uint64_t num_objs = uint64_t(objs / colls);
